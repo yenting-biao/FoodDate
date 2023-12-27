ef22083ca486f1e9726566ce2ca32c7cdf10326e
n <n>
2023-12-27 16:31:33 +0800

    solved using redirection(don't mind the undefined)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,12 @@
 "use client"
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Button, Collapse, Divider, List, ListItem, ListItemButton, ListItemText, Snackbar, TextField, Typography } from "@mui/material";
 import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 import EmailIcon from '@mui/icons-material/Email';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-
+import { useRouter } from "next/navigation";
 
 import type { AlertProps } from '@mui/material/Alert';
 import MuiAlert from '@mui/material/Alert';
@@ -19,9 +19,15 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 ) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
-
 export default function ProfilePage() {
   const { data: session } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (!session) {
+      router.push('/login');
+    }
+  }, [session]);
+
   const username = session?.user?.username;
   const email = session?.user?.email;
   const bio = "嗨大家，我是大學生小明，生活在這個充滿活力的校園裡。我熱愛探索世界，對於不同的文化和飲食都充滿好奇心。平常喜歡參與社團活動，特別是戶外運動和攝影。我認為人生短暫，應該充實自己，讓每一天都過得有趣有意義。我是一個好相處的人，喜歡和不同背景的人交流，也喜歡嚐試新的食物。如果你對於冒險、音樂和美食有興趣，我們或許可以一起探索校園附近的好吃好玩之處，成為彼此生活的一部分！";
@@ -53,7 +59,7 @@ export default function ProfilePage() {
   }
 
   const width = "w-7/12";
-  return (
+  return (<>{!!session && 
     <div className="flex flex-col items-center gap-3">
       <ProfileHeader 
         username={username}
@@ -170,6 +176,6 @@ export default function ProfilePage() {
       <div>
 
       </div>
-    </div>    
+    </div>}</>    
   );
 }
