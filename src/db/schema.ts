237005adbab09d0aefec0d8fc02eee68f06c4e43
Toshiki Ogimoto <toshiki.ogimoto@gmail.com>
2023-12-31 22:02:15 +0800

    fix(db): make name of pendingdates table lowercase

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -271,7 +271,7 @@ export const taggedRestaurantsRelations = relations(
 );
 
 export const pendingDatesTable = pgTable(
-  "pendingDates",
+  "pendingdates",
   {
     pendingDateId: uuid("pendingdateid").primaryKey().notNull().defaultRandom(),
     participantCount: integer("participantcount").notNull(),
